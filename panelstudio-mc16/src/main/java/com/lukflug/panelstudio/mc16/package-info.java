ebc30d6a491cd3626e48822f4a10fb2cfe6c921c
lukflug <lukflug@googlemail.com>
2021-01-22 19:40:06 +0100

    Corrected javadoc

--- panelstudio-mc16/src/main/java/com/lukflug/panelstudio/mc16/package-info.java
+++ panelstudio-mc16/src/main/java/com/lukflug/panelstudio/mc16/package-info.java
@@ -1,7 +1,6 @@
 /**
- * PanelStudio-MC utility functions designed for FabricMC 1.16.5.
+ * PanelStudio-MC utility functions designed for FabricMC 1.16.x.
  * This library only depends on the Fabric Yarn mappings and not on the Fabric Mod Loader or the Fabric API.
- * Could work with other Minecraft releases. However, this has not been tested.
  * @author lukflug
  */
 package com.lukflug.panelstudio.mc16;
