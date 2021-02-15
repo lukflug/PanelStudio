c2a44f98a57ca9a96c363c7ef7e17a9fffd5f540
lukflug <lukflug@googlemail.com>
2021-02-15 20:25:55 +0100

    Fixed theme multiplexer

--- panelstudio/src/main/java/com/lukflug/panelstudio/theme/ThemeMultiplexer.java
+++ panelstudio/src/main/java/com/lukflug/panelstudio/theme/ThemeMultiplexer.java
@@ -3,6 +3,7 @@ package com.lukflug.panelstudio.theme;
 import java.awt.Color;
 
 import com.lukflug.panelstudio.base.IBoolean;
+import com.lukflug.panelstudio.base.IInterface;
 
 /**
  * Base class used to enable switch themes "on-the-fly".
@@ -10,40 +11,83 @@ import com.lukflug.panelstudio.base.IBoolean;
  * In this way, the renderers can effectively be switched, without changing the field in the components itself.
  * @author lukflug
  */
-public abstract class ThemeMultiplexer extends ThemeBase {
-	/**
-	 * Initializes the renderer fields.
-	 */
-	public ThemeMultiplexer (IColorScheme scheme) {
-		super(scheme);
-		IDescriptionRendererProxy descriptionRenderer=()->getTheme().getDescriptionRenderer();
-		IButtonRendererProxy<Void> titleRenderer=()->getTheme().getTitleRenderer(false);
-		IButtonRendererProxy<Void> panelTitleRenderer=()->getTheme().getTitleRenderer(true);
-		IPanelRendererProxy panelRenderer=()->getTheme().getPanelRenderer(false);
-		IPanelRendererProxy panelPanelRenderer=()->getTheme().getPanelRenderer(true);
-		IContainerRendererProxy containerRenderer=()->getTheme().getContainerRednerer(false);
-		IContainerRendererProxy panelContainerRenderer=()->getTheme().getContainerRednerer(true);
-		IScrollBarRendererProxy scrollRenderer=()->getTheme().getScrollBarRenderer();
-		IEmptySpaceRendererProxy cornerRenderer=()->getTheme().getEmptySpaceRenderer();
-		IButtonRendererProxy<IBoolean> toggleButtonRenderer=()->getTheme().getToggleButtonRenderer();
-		IButtonRendererProxy<IBoolean> checkMarkRenderer=()->getTheme().getCheckMarkRenderer();
-		IButtonRendererProxy<String> cycleButtonRenderer=()->getTheme().getCycleButtonRenderer();
-		IButtonRendererProxy<String> keybindRenderer=()->getTheme().getKeybindRenderer();
-		ISliderRendererProxy sliderRenderer=()->getTheme().getSliderRenderer();
-		this.descriptionRenderer=descriptionRenderer;
-		this.titleRenderer=titleRenderer;
-		this.panelTitleRenderer=panelTitleRenderer;
-		this.panelRenderer=panelRenderer;
-		this.panelPanelRenderer=panelPanelRenderer;
-		this.containerRenderer=containerRenderer;
-		this.panelContainerRenderer=panelContainerRenderer;
-		this.scrollRenderer=scrollRenderer;
-		this.cornerRenderer=cornerRenderer;
-		this.toggleButtonRenderer=toggleButtonRenderer;
-		this.checkMarkRenderer=checkMarkRenderer;
-		this.cycleButtonRenderer=cycleButtonRenderer;
-		this.keybindRenderer=keybindRenderer;
-		this.sliderRenderer=sliderRenderer;
+public abstract class ThemeMultiplexer implements ITheme {
+	IDescriptionRendererProxy descriptionRenderer=()->getTheme().getDescriptionRenderer();
+	IButtonRendererProxy<Void> titleRenderer=()->getTheme().getTitleRenderer(false);
+	IButtonRendererProxy<Void> panelTitleRenderer=()->getTheme().getTitleRenderer(true);
+	IPanelRendererProxy panelRenderer=()->getTheme().getPanelRenderer(false);
+	IPanelRendererProxy panelPanelRenderer=()->getTheme().getPanelRenderer(true);
+	IContainerRendererProxy containerRenderer=()->getTheme().getContainerRednerer(false);
+	IContainerRendererProxy panelContainerRenderer=()->getTheme().getContainerRednerer(true);
+	IScrollBarRendererProxy scrollRenderer=()->getTheme().getScrollBarRenderer();
+	IEmptySpaceRendererProxy cornerRenderer=()->getTheme().getEmptySpaceRenderer();
+	IButtonRendererProxy<IBoolean> toggleButtonRenderer=()->getTheme().getToggleButtonRenderer();
+	IButtonRendererProxy<IBoolean> checkMarkRenderer=()->getTheme().getCheckMarkRenderer();
+	IButtonRendererProxy<String> cycleButtonRenderer=()->getTheme().getCycleButtonRenderer();
+	IButtonRendererProxy<String> keybindRenderer=()->getTheme().getKeybindRenderer();
+	ISliderRendererProxy sliderRenderer=()->getTheme().getSliderRenderer();
+
+	@Override
+	public void loadAssets(IInterface inter) {
+		getTheme().loadAssets(inter);
+	}
+
+	@Override
+	public IDescriptionRenderer getDescriptionRenderer() {
+		return descriptionRenderer;
+	}
+	
+	@Override
+	public IButtonRenderer<Void> getTitleRenderer(boolean panel) {
+		if (panel) return panelTitleRenderer;
+		else return titleRenderer;
+	}
+
+	@Override
+	public IPanelRenderer getPanelRenderer(boolean panel) {
+		if (panel) return panelPanelRenderer;
+		else return panelRenderer;
+	}
+
+	@Override
+	public IContainerRenderer getContainerRednerer(boolean panel) {
+		if (panel) return panelContainerRenderer;
+		else return containerRenderer;
+	}
+
+	@Override
+	public IScrollBarRenderer getScrollBarRenderer() {
+		return scrollRenderer;
+	}
+
+	@Override
+	public IEmptySpaceRenderer getEmptySpaceRenderer() {
+		return cornerRenderer;
+	}
+
+	@Override
+	public IButtonRenderer<IBoolean> getToggleButtonRenderer() {
+		return toggleButtonRenderer;
+	}
+
+	@Override
+	public IButtonRenderer<IBoolean> getCheckMarkRenderer() {
+		return checkMarkRenderer;
+	}
+
+	@Override
+	public IButtonRenderer<String> getCycleButtonRenderer() {
+		return cycleButtonRenderer;
+	}
+
+	@Override
+	public IButtonRenderer<String> getKeybindRenderer() {
+		return keybindRenderer;
+	}
+
+	@Override
+	public ISliderRenderer getSliderRenderer() {
+		return sliderRenderer;
 	}
 
 	@Override
