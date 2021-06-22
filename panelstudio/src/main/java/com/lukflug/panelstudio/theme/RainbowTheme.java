e6550db313c0ef873d73d466ed25b55d705fc814
lukflug <lukflug@googlemail.com>
2021-06-22 18:46:03 +0200

    Almost down with Rainbow Theme

--- panelstudio/src/main/java/com/lukflug/panelstudio/theme/RainbowTheme.java
+++ panelstudio/src/main/java/com/lukflug/panelstudio/theme/RainbowTheme.java
@@ -29,6 +29,7 @@ public class RainbowTheme extends ThemeBase {
 		scheme.createSetting(this,"Rainbow Color","The rainbow base color.",false,true,new Color(255,0,0),false);
 		scheme.createSetting(this,"Background Color","The main color for disabled components.",false,true,new Color(64,64,64),false);
 		scheme.createSetting(this,"Font Color","The main color for text.",false,true,new Color(255,255,255),false);
+		scheme.createSetting(this,"Highlight Color","The color for highlighted text.",false,true,new Color(0,0,255),false);
 	}
 	
 	protected void renderOverlay (Context context) {
@@ -105,14 +106,28 @@ public class RainbowTheme extends ThemeBase {
 		return new IScrollBarRenderer<T>() {
 			@Override
 			public int renderScrollBar(Context context, boolean focus, T state, boolean horizontal, int height, int position) {
-				// TODO Auto-generated method stub
-				return 0;
+				Color color=getBackgroundColor(focus);
+				if (graphicalLevel==0 || buttonRainbow.isOn()) {
+					renderRainbowRect(context.getRect(),context,focus);
+				}
+				if (horizontal) {
+					int a=(int)(position/(double)height*context.getSize().width);
+					int b=(int)((position+context.getSize().width)/(double)height*context.getSize().width);
+					context.getInterface().fillRect(new Rectangle(context.getPos().x,context.getPos().y,a,context.getSize().height),color,color,color,color);
+					context.getInterface().fillRect(new Rectangle(context.getPos().x+b,context.getPos().y,context.getSize().width-b,context.getSize().height),color,color,color,color);
+				} else {
+					int a=(int)(position/(double)height*context.getSize().height);
+					int b=(int)((position+context.getSize().height)/(double)height*context.getSize().height);
+					context.getInterface().fillRect(new Rectangle(context.getPos().x,context.getPos().y,context.getSize().width,a),color,color,color,color);
+					context.getInterface().fillRect(new Rectangle(context.getPos().x,context.getPos().y+b,context.getSize().width,context.getSize().height-b),color,color,color,color);
+				}
+				if (horizontal) return (int)((context.getInterface().getMouse().x-context.getPos().x)*height/(double)context.getSize().width-context.getSize().width/2.0);
+				else return (int)((context.getInterface().getMouse().y-context.getPos().y)*height/(double)context.getSize().height-context.getSize().height/2.0);
 			}
 
 			@Override
 			public int getThickness() {
-				// TODO Auto-generated method stub
-				return 0;
+				return 4;
 			}
 		};
 	}
@@ -122,8 +137,8 @@ public class RainbowTheme extends ThemeBase {
 		return new IEmptySpaceRenderer<T>() {
 			@Override
 			public void renderSpace(Context context, boolean focus, T state) {
-				// TODO Auto-generated method stub
-				
+				Color color=getBackgroundColor(focus);
+				context.getInterface().fillRect(context.getRect(),color,color,color,color);
 			}
 		};
 	}
@@ -206,14 +221,25 @@ public class RainbowTheme extends ThemeBase {
 		return new IRadioRenderer() {
 			@Override
 			public void renderItem(Context context, ILabeled[] items, boolean focus, int target, double state, boolean horizontal) {
-				// TODO Auto-generated method stub
-				
+				if (graphicalLevel==0 || buttonRainbow.isOn()) {
+					renderRainbowRect(context.getRect(),context,focus);
+				}
+				for (int i=0;i<items.length;i++) {
+					Rectangle rect=getItemRect(context,items,i,horizontal);
+					Context subContext=new Context(context.getInterface(),rect.width,rect.getLocation(),context.hasFocus(),context.onTop());
+					subContext.setHeight(rect.height);
+					if (i!=target) {
+						Color color=getBackgroundColor(focus);
+						context.getInterface().fillRect(subContext.getRect(),color,color,color,color);
+					}
+					renderOverlay(subContext);
+					context.getInterface().drawString(new Point(rect.x+padding,rect.y+padding),height,items[i].getDisplayName(),getFontColor(focus));
+				}
 			}
 
 			@Override
 			public int getDefaultHeight(ILabeled[] items, boolean horizontal) {
-				// TODO Auto-generated method stub
-				return 0;
+				return (horizontal?1:items.length)*getBaseHeight();
 			}
 		};
 	}
@@ -223,14 +249,17 @@ public class RainbowTheme extends ThemeBase {
 		return new IResizeBorderRenderer() {
 			@Override
 			public void drawBorder(Context context, boolean focus) {
-				// TODO Auto-generated method stub
-				
+				Color color=getBackgroundColor(focus);
+				Rectangle rect=context.getRect();
+				context.getInterface().fillRect(new Rectangle(rect.x,rect.y,rect.width,getBorder()),color,color,color,color);
+				context.getInterface().fillRect(new Rectangle(rect.x,rect.y+rect.height-getBorder(),rect.width,getBorder()),color,color,color,color);
+				context.getInterface().fillRect(new Rectangle(rect.x,rect.y+getBorder(),getBorder(),rect.height-2*getBorder()),color,color,color,color);
+				context.getInterface().fillRect(new Rectangle(rect.x+rect.width-getBorder(),rect.y+getBorder(),getBorder(),rect.height-2*getBorder()),color,color,color,color);
 			}
 
 			@Override
 			public int getBorder() {
-				// TODO Auto-generated method stub
-				return 0;
+				return 2;
 			}
 		};
 	}
@@ -239,26 +268,89 @@ public class RainbowTheme extends ThemeBase {
 	public ITextFieldRenderer getTextRenderer(boolean embed, int logicalLevel, int graphicalLevel, boolean container) {
 		return new ITextFieldRenderer() {
 			@Override
-			public int renderTextField(Context context, String title, boolean focus, String content, int position, int select, int boxPosition, boolean insertMode) {
-				// TODO Auto-generated method stub
-				return 0;
+			public int renderTextField (Context context, String title, boolean focus, String content, int position, int select, int boxPosition, boolean insertMode) {
+				boolean effFocus=container?context.hasFocus():focus;
+				if (graphicalLevel==0 || buttonRainbow.isOn()) {
+					renderRainbowRect(context.getRect(),context,effFocus);
+				}
+				// Declare and assign variables
+				Color textColor=getFontColor(effFocus);
+				Color highlightColor=scheme.getColor("Highlight Color");
+				Rectangle rect=getTextArea(context,title);
+				int strlen=context.getInterface().getFontWidth(height,content.substring(0,position));
+				context.getInterface().fillRect(rect,new Color(0,0,0,64),new Color(0,0,0,64),new Color(0,0,0,64),new Color(0,0,0,64));
+				ITheme.drawRect(context.getInterface(),rect,new Color(0,0,0,64));
+				// Deal with box render offset
+				if (boxPosition<position) {
+					int minPosition=boxPosition;
+					while (minPosition<position) {
+						if (context.getInterface().getFontWidth(height,content.substring(0,minPosition))+rect.width-padding>=strlen) break;
+						minPosition++;
+					}
+					if (boxPosition<minPosition) boxPosition=minPosition;
+				} else if (boxPosition>position) boxPosition=position-1;
+				int maxPosition=content.length();
+				while (maxPosition>0) {
+					if (context.getInterface().getFontWidth(height,content.substring(maxPosition))>=rect.width-padding) {
+						maxPosition++;
+						break;
+					}
+					maxPosition--;
+				}
+				if (boxPosition>maxPosition) boxPosition=maxPosition;
+				else if (boxPosition<0) boxPosition=0;
+				int offset=context.getInterface().getFontWidth(height,content.substring(0,boxPosition));
+				// Deal with highlighted text
+				int x1=rect.x+padding/2-offset+strlen;
+				int x2=rect.x+padding/2-offset;
+				if (position<content.length()) x2+=context.getInterface().getFontWidth(height,content.substring(0,position+1));
+				else x2+=context.getInterface().getFontWidth(height,content+"X");
+				// Draw stuff around the box
+				renderOverlay(context);
+				context.getInterface().drawString(new Point(context.getPos().x+padding,context.getPos().y+padding/2),height,title+separator,textColor);
+				// Draw the box
+				context.getInterface().window(rect);
+				if (select>=0) {
+					int x3=rect.x+padding/2-offset+context.getInterface().getFontWidth(height,content.substring(0,select));
+					context.getInterface().fillRect(new Rectangle(Math.min(x1,x3),rect.y+padding/2,Math.abs(x3-x1),height),highlightColor,highlightColor,highlightColor,highlightColor);
+				}
+				context.getInterface().drawString(new Point(rect.x+padding/2-offset,rect.y+padding/2),height,content,textColor);
+				if ((System.currentTimeMillis()/500)%2==0 && focus) {
+					if (insertMode) context.getInterface().fillRect(new Rectangle(x1,rect.y+padding/2+height,x2-x1,1),textColor,textColor,textColor,textColor);
+					else context.getInterface().fillRect(new Rectangle(x1,rect.y+padding/2,1,height),textColor,textColor,textColor,textColor);
+				}
+				context.getInterface().restore();
+				return boxPosition;
 			}
 
 			@Override
 			public int getDefaultHeight() {
-				return getBaseHeight();
+				int height=getBaseHeight()-padding;
+				if (height%2==1) height+=1;
+				return height;
 			}
 
 			@Override
-			public Rectangle getTextArea(Context context, String title) {
-				// TODO Auto-generated method stub
-				return context.getRect();
+			public Rectangle getTextArea (Context context, String title) {
+				Rectangle rect=context.getRect();
+				int length=padding+context.getInterface().getFontWidth(height,title+separator);
+				return new Rectangle(rect.x+length,rect.y,rect.width-length,rect.height);
 			}
 
 			@Override
 			public int transformToCharPos(Context context, String title, String content, int boxPosition) {
-				// TODO Auto-generated method stub
-				return 0;
+				Rectangle rect=getTextArea(context,title);
+				Point mouse=context.getInterface().getMouse();
+				int offset=context.getInterface().getFontWidth(height,content.substring(0,boxPosition));
+				if (rect.contains(mouse)) {
+					for (int i=1;i<=content.length();i++) {
+						if (rect.x+padding/2-offset+context.getInterface().getFontWidth(height,content.substring(0,i))>mouse.x) {
+							return i-1;
+						}
+					}
+					return content.length();
+				}
+				return -1;
 			}
 		};
 	}
