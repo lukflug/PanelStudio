0299801c03be944556637603e09153749b48c276
lukflug <lukflug@googlemail.com>
2022-03-09 14:48:35 +0100

    Fixed issues with example mods for 1.17 and 1.18

--- panelstudio-mc17/src/main/java/com/lukflug/panelstudio/mc17/package-info.java
+++ panelstudio-mc17/src/main/java/com/lukflug/panelstudio/mc17/package-info.java
@@ -1,5 +1,5 @@
 /**
- * PanelStudio-MC utility functions designed for FabricMC 1.17.x.
+ * PanelStudio-MC utility functions designed for FabricMC 1.17.x and 1.18.x.
  * This library only depends on the Fabric Yarn mappings and not on the Fabric Mod Loader or the Fabric API.
  * @author lukflug
  */
