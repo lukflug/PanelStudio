40f2f34e797fe70430deb48253269e5a6ece0ee8
Diliard <43886528+Diliard@users.noreply.github.com>
2022-10-23 00:55:37 +0200

    Delete panelstudio-mc19/src/main/java/com/lukflug/panelstudio/mc17 directory

--- panelstudio-mc19/src/main/java/com/lukflug/panelstudio/mc17/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * PanelStudio-MC utility functions designed for FabricMC 1.17.x and 1.18.x.
- * This library only depends on the Fabric Yarn mappings and not on the Fabric Mod Loader or the Fabric API.
- * @author lukflug
- */
-package com.lukflug.panelstudio.mc17;
